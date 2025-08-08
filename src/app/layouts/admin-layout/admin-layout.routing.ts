abf13dccacce2ea7e07daa6056d1b9817b37a722
jeifermarrugo24 <jeifermarrugo0124@gmail.com>
2025-08-07 23:00:50 -0500

    arreglo vistas

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -7,6 +7,10 @@ import { ProductsComponent } from "../../products/products.component";
 import { RegistrarComponent as UserRegistrarComponent } from "../../users/registrar/registrar.component";
 import { PermisosComponent } from "../../permisos/permisos.component";
 import { MenusComponent } from "app/menus/menus.component";
+import { SubcategoriesComponent as SubCategoriesComponent } from "app/subcategories/subcategories.component";
+import { RegistrarCategoriesComponent } from "app/categories/registrar-categories/registrar-categories.component";
+import { RegistrarProductsComponent } from "app/products/registrar-products/registrar-products.component";
+import { RegistrarSubcategoriesComponent } from "app/subcategories/registrar-subcategories/registrar-subcategories.component";
 
 export const AdminLayoutRoutes: Routes = [
   { path: "", redirectTo: "dashboard", pathMatch: "full" },
@@ -14,7 +18,11 @@ export const AdminLayoutRoutes: Routes = [
   { path: "users", component: UsersComponent },
   { path: "users/registrar", component: UserRegistrarComponent },
   { path: "categories", component: CategoriesComponent },
+  { path: "categories/registrar", component: RegistrarCategoriesComponent },
+  { path: "subcategories", component: SubCategoriesComponent },
+  { path: "subcategories/registrar", component: RegistrarSubcategoriesComponent },
   { path: "products", component: ProductsComponent },
+  { path: "products/registrar", component: RegistrarProductsComponent },
   { path: "menus", component: DashboardComponent },
   { path: "roles", component: DashboardComponent },
   { path: "permisos", component: PermisosComponent },
